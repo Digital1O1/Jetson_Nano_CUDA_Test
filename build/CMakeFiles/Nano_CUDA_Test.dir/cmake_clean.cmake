20fd67bb33140b3eb8974fb8fcf70d7dff0e9389
Digital1O1 <ChristopherTa89@gmail.com>
2023-08-16 22:34:14 -0500

    recent changes

---
+++ build/CMakeFiles/Nano_CUDA_Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nano_CUDA_Test.dir/Nano_CUDA_Test.cpp.o"
+  "Nano_CUDA_Test"
+  "Nano_CUDA_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nano_CUDA_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ctnano/Desktop/Coding_Folder/Jetson_Nano_CUDA_Test
+# Build directory: /home/ctnano/Desktop/Coding_Folder/Jetson_Nano_CUDA_Test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
